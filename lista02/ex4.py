62ae47c45a7d44c87bb850179bf97890ea44f019
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-08-23 01:59:12 -0300

    Update ex4.py

--- lista02/ex4.py
+++ lista02/ex4.py
@@ -3,20 +3,25 @@ b = float(input())
 c = float(input())
 
 d = b**2 - 4*a*c
-if(d > 0):
-    x1 = (-b + d**(1/2))/(2*a)
-    x2 = (-b - d**(1/2))/(2*a)
-    if(x1>x2):
-        temp = x2
-        x2 = x1
-        x1 = temp
-    print(f"As raízes são {x1} e {x2}")
-    
-if(d==0):
-    x1 = (-b + d**(1/2))/(2*a)
-    print(f"A raiz é {x1}")
 
-if(d<0):
-    print("Nenhuma raiz")
+if(a == 0):
+    x1 = -c/b
+    print(f"A raiz é {x1}")
+else:
+    if(d > 0):
+        x1 = (-b + d**(1/2))/(2*a)
+        x2 = (-b - d**(1/2))/(2*a)
+        if(x1>x2):
+            temp = x2
+            x2 = x1
+            x1 = temp
+        print(f"As raízes são {x1} e {x2}")
+        
+    if(d==0):
+        x1 = (-b + d**(1/2))/(2*a)
+        print(f"A raiz é {x1}")
+
+    if(d<0):
+        print("Nenhuma raiz")
 
 
