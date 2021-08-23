5ca24cf62209dff768aaeaae0a6d3d2e4955c567
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-08-23 01:05:11 -0300

    Create ex1.py

---
+++ lista02/ex1.py
@@ -0,0 +1,6 @@
+qbola = int(input("qnt bola: "))
+pbola = float(input("preço bola: "))
+qchocolate = int(input("qnt chocolate: "))
+pchocolate = float(input("preço chocolate: "))
+print(f"O preço total é: {(qbola*pbola+qchocolate*pchocolate):.2f} R$")
+
