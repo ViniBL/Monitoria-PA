05cbaf409cebbb8781a7c836d30024a90ad87c3e
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-09-20 17:09:01 -0300

    Update ex3.py

--- lista05/ex3.py
+++ lista05/ex3.py
@@ -7,16 +7,20 @@ while True:
     
 n_l = int(input())
 n_c = int(input())
+if n_l*n_c != len(l):
+    print('IMPOSSÍVEL')
+else:
+    
+    m = [[0 for c in range(n_c)] for r in range(n_l)]
 
-m = [[0 for c in range(n_c)] for r in range(n_l)]
-pos = 0
-for i in range(n_l):
-    for j in range(n_c):
-        m[i][j] = l[pos]
-        pos += 1
-        
-for i in range(n_l):
-    for j in range(n_c):
-        print(m[i][j],end=' ')
-    print('')
+    pos = 0
+    for i in range(n_l):
+        for j in range(n_c):
+            m[i][j] = l[pos]
+            pos += 1
+            
+    for i in range(n_l):
+        for j in range(n_c):
+            print(m[i][j],end=' ')
+        print('')
 
