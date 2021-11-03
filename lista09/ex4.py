559223f365c4cb160af8bc1b2df4876ecfc5afab
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-11-03 17:27:21 -0300

    Add files via upload

--- lista09/ex4.py
+++ lista09/ex4.py
@@ -1,2 +1,13 @@
-def bit(x, n):
-    return x & n+1
+def bit(x,n):
+    count = 0
+    if(x>=0):
+        x = bin(x)[2:]
+    else:
+        x = bin(x)[3:]
+        
+    x = list(map(int, str(x)))
+    for i in reversed(x):
+        if count == n:
+            return i
+        count += 1
+        
