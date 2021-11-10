3a5c0580557d27cf6e3b191bb646d57fe10546af
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-11-09 21:13:02 -0300

    Add files via upload

--- lista10/ex1.py
+++ lista10/ex1.py
@@ -1,7 +1,7 @@
-def soma(m1, m2):
+import numpy as np
+def soma(m1=np.random.rand(3,2), m2=np.random.rand(3,2)):
     try:
         return np.add(m1,m2)
     except ValueError:
         print("Tamanhos inconsistentes")
         
-
