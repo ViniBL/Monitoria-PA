ffe5c33a6b6336a958772c7c688ecd15e452bd1a
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-11-09 21:56:25 -0300

    Add files via upload

--- lista10/ex1.py
+++ lista10/ex1.py
@@ -3,11 +3,15 @@ def soma(m1=0, m2=0):
     
     if type(m1) == 'int':
         m1 = np.random.rand(m2.shape[1:-1])
+        print("alo")
     if type(m1) == 'int':
         m2 = np.random.rand(m1.shape[1:-1])
+        print("alo")
         
-    try:
-        return np.add(m1,m2)
-    except ValueError:
-        print("Tamanhos inconsistentes")
+    if(m1.shape == m2.shape):
+        return m1 + m2
+    else:
+        raise ValueError("Tamanhos inconsistentes")
+        
+
         
