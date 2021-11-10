0fa6a174ad8929100959dc2df816903fe47fd3dd
ViniBL <86486760+ViniBL@users.noreply.github.com>
2021-11-09 21:38:32 -0300

    Add files via upload

--- lista10/ex1.py
+++ lista10/ex1.py
@@ -1,5 +1,11 @@
 import numpy as np
-def soma(m1=np.random.rand(3,2), m2=np.random.rand(3,2)):
+def soma(m1=0, m2=0):
+    
+    if type(m1) == 'int':
+        m1 = np.random.rand(m2.shape[1:-1])
+    if type(m1) == 'int':
+        m2 = np.random.rand(m1.shape[1:-1])
+        
     try:
         return np.add(m1,m2)
     except ValueError:
